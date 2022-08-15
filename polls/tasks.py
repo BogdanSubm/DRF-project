7c658aebd9a715c692dd49a5d8f4ef55a60c5597
Bogdan <bogdan.vp2015@yandex.ru>
2022-08-15 18:30:56 +0300

    upgrade tasks

--- polls/tasks.py
+++ polls/tasks.py
@@ -4,20 +4,19 @@ from datetime import datetime
 import requests
 import json
 
+from django.utils import timezone
 from mailing.celery import app
 from .models import Client, Mailing, Message
 from django.shortcuts import get_object_or_404
 
 @app.task
 def send_verification_msg(mailing_id):
-    mailing = Mailing.objects.filter(id=mailing_id)
-    clients = Client.objects.filter(tag=mailing.get("property_filter"))
-    mailing_start = datetime.strptime(mailing.get('mailing_start'), '%Y-%m-%dT%H:%M:%SZ')
-    mailing_end = datetime.strptime(mailing.get('mailing_end'), '%Y-%m-%dT%H:%M:%SZ')
-    datetime_now = datetime.now()
-    keep_running = datetime_now > mailing_start and datetime_now < mailing_end
+    mailing = Mailing.objects.get(id=mailing_id)
+    clients = Client.objects.filter(tag=mailing.tag)
+    datetime_now = timezone.now()
+    keep_running = datetime_now > mailing.mailing_start and datetime_now < mailing.mailing_end
     if keep_running:
-        msg = Message(msg_start=datetime_now, msg_status=0, id_mailing=mailing_id)
+        msg = Message(msg_start=datetime_now, msg_status=0, id_mailing=mailing)
         try:
             for client in clients:
                 msg.id_client = client.get('id')
@@ -28,7 +27,7 @@ def send_verification_msg(mailing_id):
                 body = {
                     "id": msg.id,
                     "phone": client.get("phone"),
-                    "text": mailing.get("msg_text")
+                    "text": mailing.msg_text
                 }
                 responce = requests.post(f"https://probe.fbrq.cloud/v1/send/{msg.id}", headers=headers,
                                          data=json.dumps(body))
